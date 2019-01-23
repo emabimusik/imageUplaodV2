df438a0871c8b0bf7ceefb57f1a66000e6c31430
Karina Rothoff Brix <karinarothoffbrix@Karinas-iMac.local>
2019-01-23 15:22:10 +0100

    This is the fourth commit

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
     imageLoader.addEventListener('change', handleImage, false);
     var canvas = document.getElementById('imageCanvas');
     var ctx = canvas.getContext('2d');
+    var dwn = document.getElementById('btndownload');
 
-
-
+        dwn.style.visibility="hidden";
 
     // create the second context
     var canvas2 = document.getElementById('tutorial2');
@@ -15,32 +15,50 @@
     function handleImage(e){
         var reader = new FileReader();
         reader.onload = function(event){
-            var dataUrl;
+           // var dataUrl;
+            var file;
             var img = new Image();
+
             img.onload = function(){
-                canvas.width = '600';
-                canvas.height = '500';
-                ctx.translate(img.width,0)
-                ctx.drawImage(img,0,0);
-                ctx.scale(-1,1);
-                ctx.drawImage(img,0,0);
-                ctx.translate(img.width-1, img.height-1);
-                ctx.rotate(Math.PI);
-                ctx.drawImage(img,img.width,-img.height, img.width, img.height);
-                ctx.translate(img.width, 0);
-                ctx.scale(-1, 1);
-                ctx.drawImage(img,0,-img.height, img.width, img.height);
 
-                // save the image from the locastorage
+                    console.log(this.width);
+                    console.log(this.height);
+                // validate the image size
+                   if(this.width > 200 && this.height > 150){
 
-                localStorage.setItem( "savedImageData", canvas.toDataURL("image/jpg") );
+                       document.getElementById("p1").innerHTML = " Billedet er for stort til at skabe dit design. Det skal være : 200 X 150 px";
+                      //alert("Billedet er for stort til at skabe dit design. Det skal være : 200 X 150 px")
+                       dwn.style.visibility="hidden";
 
-                // get the image form the local storage
+                   }else{
+                       dwn.style.visibility="visible";
 
-                var strDataURI = localStorage.getItem('savedImageData');
-                // call the function from the drawDataUrIOnCanvas by passing the new localstorage data and the second context
+                       document.getElementById("p1").innerHTML="";
+                       canvas.width = '600';
+                       canvas.height = '500';
+                       ctx.translate(img.width,0)
+                       ctx.drawImage(img,0,0);
+                       ctx.scale(-1,1);
+                       ctx.drawImage(img,0,0);
+                       ctx.translate(img.width-1, img.height-1);
+                       ctx.rotate(Math.PI);
+                       ctx.drawImage(img,img.width,-img.height, img.width, img.height);
+                       ctx.translate(img.width, 0);
+                       ctx.scale(-1, 1);
+                       ctx.drawImage(img,0,-img.height, img.width, img.height);
 
-                drawDataURIOnCanvas(strDataURI, ctx2);
+                       // save the image from the localstorage
+
+                       localStorage.setItem( "savedImageData", canvas.toDataURL("image/jpg") );
+
+                       // get the image form the local storage
+
+                       var strDataURI = localStorage.getItem('savedImageData');
+                       // call the function from the drawDataUrIOnCanvas by passing the new localstorage data and the second context
+
+                       drawDataURIOnCanvas(strDataURI, ctx2);
+                   }
+
 
             }
             img.src = event.target.result;
@@ -78,7 +96,32 @@
 
     }
 
+  // downlaod Canvas
+    dwn.onclick = function(){
+        download(canvas2, 'minNytBilled.png');
+    }
 
+    function  download(canvas, filename) {
+        /// create an "off-screen" anchor tag
+        var lnk = document.createElement('a'), e;
+        /// the key here is to set the download attribute of the a tag
+        lnk.download = filename;
+        /// convert canvas content to data-uri for link. When download
+        /// attribute is set the content pointed to by link will be
+        /// pushed as "download" in HTML5 capable browsers
+        lnk.href = canvas.toDataURL("image/png;base64");
+        /// create a "fake" click-event to trigger the download
+        if (document.createEvent) {
+            e = document.createEvent("MouseEvents");
+            e.initMouseEvent("click", true, true, window,
+                0, 0, 0, 0, 0, false, false, false,
+                false, 0, null);
 
+            lnk.dispatchEvent(e);
+        } else if (lnk.fireEvent) {
+            lnk.fireEvent("onclick");
+        }
+
+    }
 
 
